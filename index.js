39533b6c4b0b07475ed863237ae3c3fbb01dd43f
Rizvi Iqubal <rizviiqubal@gmail.com>
2016-08-29 04:19:28 +0400

    Handling is echo

--- index.js
+++ index.js
@@ -26,17 +26,10 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        /*if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }*/
           if(event.message.is_echo){
             console.log("Message is echo");
-            continue;
+            //continue;
           }
-          console.log("Event:"+JSON.stringify(event));
-
-
-
           if (event.message && event.message.text) {
                   //sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
           }
